c0ed3d2af1502dbd13c2d546929fe33116bd9c8a
twof <fabiobean2@gmail.com>
2016-05-23 18:53:01 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  CdTLang
+//
+//  Created by fnord on 5/23/16.
+//  Copyright © 2016 fnord. All rights reserved.
+//
+#define same for
+#define tbh true
+#define uj cout
+#define ama cin
+#define gg }
+#define trashDumpyard delete
+#define doGirlsLikeThis if
+#define unilateral break
+#define okayThanksForLettingUsKnow return
+#define uninvested ;
+#define cancer while
+#define friendOfMine friend
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    same(int i = 0;i<10;i++){
+        doGirlsLikeThis(i<3 == tbh){
+            uj << "Hello World" uninvested
+        gg else{
+            unilateral uninvested
+        gg
+    gg
+    okayThanksForLettingUsKnow 0 uninvested
+gg
